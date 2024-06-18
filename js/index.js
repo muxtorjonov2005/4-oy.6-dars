6d94727e220f54aff1e3f6ee969ef80652dedddd
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-06-18 12:17:08 +0500

    first coomit

---
+++ js/index.js
@@ -0,0 +1,287 @@
+// 1-masala
+// function sumTwoNumbers(arg1, arg2){
+//     console.log(arg1 + arg2);
+// }
+// sumTwoNumbers(a, b)
+
+// 2-masala
+// function kopTwoNumbers(arg1, arg2){
+//     console.log(arg1 * arg2);
+// }
+// kopTwoNumbers(a, b)
+
+// 3-masala
+// function kvNumber(arg){
+//     console.log(arg * arg);
+// }
+// kvNumber(a)
+
+// 4-masala
+// function kubNumber(arg) {
+//     console.log(arg ** 3);
+// }
+// kubNumber(a)
+
+// 5-masala
+// function midThreeNumbers(arg1, arg2, arg3){
+//     console.log((arg1 + arg2 + arg3) / 3);
+// }
+// midThreeNumbers(a, b, c)
+
+// 6-masala
+// function num(arg) {
+//     if (arg % 2 == 0){
+//         console.log('true');
+//     } else{
+//         console.log('false');
+//     }
+// }
+// num(a)
+
+// 7-masala
+// function num(arg) {
+//     if (arg % 2 == 1){
+//         console.log('true');
+//     } else{
+//         console.log('false');
+//     }
+// }
+// num(a)
+
+// 8-masala
+// function maxTwoNumbers(arg1, arg2){
+//     let max;
+//     if (arg1 > arg2 ) {
+//         max = arg1
+//     } else {
+//         max = arg2
+//     }
+//     console.log(max);
+// }
+// maxTwoNumbers(a, b)
+
+// 9-masala
+// function minTwoNumbers(num1, num2) {
+//     let min;
+//     if (num1 > num2 ){
+//         min = num2
+//     } else{
+//         min = num1
+//     }
+//     console.log(min);
+// }
+// minTwoNumbers(a, b)
+
+// 10-masala
+// function num(n){
+//     let sum = 0
+//     for (i = 1; i <= n; i++) {
+//         sum += i;
+//     }
+//     console.log(sum);
+// }
+// num(a)
+
+// 11-masala
+
+// 12-masala
+// function age(num){
+//     if (num >= 18){
+//         console.log('katta');
+//     } else{
+//         console.log('kichik');
+//     }
+// }
+// age(19)
+
+// 13-masala
+// function numTwo(num1, num2){
+//     if (num1 % num2 == 0){
+//         console.log('true');
+//     } else {
+//         console.log('false');
+//     }
+// }
+// numTwo(a, b)
+
+// 14-masal
+// function num(n){
+//     if(n > 0){
+//         console.log('true');
+//     } else {
+//         console.log('false');
+//     }
+// }
+// num(a)
+
+// 15-masala
+// function num(n){
+//     if(n < 0){
+//         console.log('true');
+//     } else {
+//         console.log('false');
+//     }
+// }
+// num(a)
+
+// 16-masala
+
+// 17-masala
+
+// 18-masala
+// function maxThreeNumbers(num1, num2, num3) {
+//     let max 
+//     if(num1 > num2){
+//         max = num1
+//     } else {
+//         max = num2
+//     }
+//     if (max > num3){
+
+//     } else {
+//         max = num3
+//     }
+//     console.log(max);
+// }
+// maxThreeNumbers(a, b, c)
+
+// 19-masala
+// function maxThreeNumbers(num1, num2, num3) {
+//     let min 
+//     if(num1 > num2){
+//         min = num2
+//     } else {
+//         min = num1
+//     }
+//     if (min < num3){
+
+//     } else {
+//         min = num3
+//     }
+//     console.log(min);
+// }
+// maxThreeNumbers(a, b, c)
+
+// 20-masala
+// function num(n){
+//     if(n > 10){
+//         console.log('katta');
+//     } else{
+//         console.log('kichik');
+//     }
+// }
+// num(a)
+
+// 21-masala
+// function twoNumbers(num1, num2){
+//     if((num1 + num2) % 2 == 0){
+//         console.log('true');
+//     } else{
+//         console.log('false');
+//     }
+// }
+// twoNumbers(a, b)
+
+// 22-masala
+// function num(n){
+//     if(n % 5 == 0){
+//         console.log('true');
+//     } else{
+//         console.log('false');
+//     }
+// }
+// num
+
+// 23-masala
+// function twoNumbers(num1, num2){
+//     if(num1 > num2){
+//         console.log('true');
+//     } else{
+//         console.log('false');
+//     }
+// }
+// twoNumbers(a, b)
+
+// 24-masala
+// function num(n){
+//     if(n % 7 == 0){
+//         console.log(true);
+//     } else{
+//         console.log(false);
+//     }
+// }
+// num(a)
+
+// 25-masala
+// function num(n){
+//     console.log(n ** 10);
+// }
+// num(a)
+
+// 26-masala
+// function twoNumbers(num1, num2){
+//     console.log((num1 + num2) / 2);
+// }
+// twoNumbers(a, b)
+
+// 27-masala
+// function num(n){
+//     if (n < 100){
+//         console.log('kichik');
+//     } else{
+//         console.log('katta');
+//     }
+// }
+// num(a)
+
+// 28-masala
+// function num(n){
+//     if (n % 2 == 0){
+//         console.log(true);
+//     } else{
+//         console.log(false);
+//     }
+// }
+// num(a)
+
+// 29-masala
+
+// 30-masala
+// function threeNumbers(num1, num2, num3){
+//     if(num1 + num2 == num3){
+//         console.log(true);
+//     } else{
+//         console.log(false);
+//     }
+// }
+// threeNumbers(a, b, c)
+
+// 31-masaala
+// let sum = 0
+// for (let i = 1; i <= 10; i++){
+//     console.log(sum += i);
+// }
+
+// 32-masala
+// let res = 1
+// for (let i = 1; i <= 5; i++){
+//     console.log(res *= i);
+// }
+
+// 33-masala
+
+// 34-masala
+// let n = +prompt('son kirit')
+// for (let i = 1; i <= n; i++){
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+// 35-masala
+// let n = +prompt('son kirit')
+// for (let i = 1; i <= n; i++){
+//     if (i % 2 == 1){
+//         console.log(i);
+//     }
+// }
